0acfed350e55dd0873dd11e0900244641419cada
Warren Siro <warrensiro@gmail.com>
2025-12-18 23:30:19 +0300

    structuring my db schema

---
+++ models/user.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: [true, ""], // we can customize the message here in the string
+  },
+  lastName: {
+    type: String,
+    required: [true, ""],
+  },
+  avatar: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: [true, ""],
+    validate: {
+      validator: function (email) {
+        return String(email)
+          .toLowerCase()
+          .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          );
+      },
+      message: (props) => `Email (${props.value}) is invalid`,
+    },
+  },
+  password: {
+    type: String,
+  },
+  passwordChangedAt: {
+    type: Date,
+  },
+  passwordResetToken: {
+    type: String,
+  },
+  passwordResetExpires: {
+    type: Date,
+  },
+  createdAt: {
+    type: Date,
+  },
+  updatedAt: {
+    type: Date,
+  },
+});
