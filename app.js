b9ad107157643b9d6194a002cea41133171e36f6
Warren Siro <warrensiro@gmail.com>
2025-12-18 22:59:17 +0300

    setting up DB cnnection

--- app.js
+++ app.js
@@ -10,6 +10,45 @@ const mongoSanitize = require('express-mongo-sanitize') // prevent nosql injecti
 
 const bodyParser = require('body-parser') // parse request body
 
+const xss = require('xss') //prevent xss attacks
+
+const cors = require("cors")
+
 const app = express()
+
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(mongoSanitize())
+
+// app.use(xss())
+
+// next is to add other middlewares1
+app.use(cors({
+    origin: 'x',
+    methods: ["GET", "PATCH", "PUT", "DELETE", "POST"],
+    credentials: true,
+}))
+
+app.use(express.json({ limit: "10kb" }))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ encoded: true}))
+app.use(helmet())
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+// to avoid crashing the server with too many requests
+const limiter = rateLimit({
+    max: 1000,
+    windowMs: 60 * 60 * 1000,
+    message: "Try again in an hour"
+})
+
+app.use('/siro', limiter) // apply rate limiting to all requests starting with /siro
+
+
 
 module.exports = app
--- server.js
+++ server.js
@@ -1,11 +1,43 @@
-const app = require('./app')
+const app = require("./app");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config({ path: "./config.env" });
 
-const http = require('http')
+// error handling for uncaught exceptions and unhandled promise rejections
+process.on("uncaughtException", (err) => {
+  console.log(err);
+  process.exit(1);
+});
 
-const server = http.createServer(app)
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  server.close(() => {
+    process.exit(1);
+  });
+});
 
-const port = process.env.PORT || 8000
+const http = require("http");
+
+const server = http.createServer(app);
+
+const DB = process.env.DURI.replace("PASSWORD", process.env.DBPASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB connection successful");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const port = process.env.PORT || 8000;
 
 server.listen(port, () => {
-    console.log(`App is running on port ${port}`)
-})
+  console.log(`App is running on port ${port}`);
+});
