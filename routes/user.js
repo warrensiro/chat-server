7dca70e31cb217adb869fac90e2d7f30223483f3
Warren Siro <warrensiro@gmail.com>
2026-01-01 18:29:15 +0300

    sending and receiving socket events

--- routes/user.js
+++ routes/user.js
@@ -5,4 +5,6 @@ const authController = require("../controllers/auth");
 
 router.patch("/update-me", authController.protect, userController.updateMe);
 
+router.post("/get-users", authController.protect, userController.getUsers)
+
 module.exports = router;
