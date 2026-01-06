c3dec6939a4efedac629e9472416c08a234e8c39
Warren Siro <warrensiro@gmail.com>
2026-01-06 20:49:08 +0300

    message sending in backend

--- server.js
+++ server.js
@@ -139,28 +139,52 @@ io.on("connection", async (socket) => {
         "participants",
         "firstName lastName _id email status"
       );
-      console.log(new_chat)
+      console.log(new_chat);
 
-      socket.emit("start_chat", new_chat)
-
-    }
-
-    else {
-      socket.emit("open_chat", existing_conversation[0])
+      socket.emit("start_chat", new_chat);
+    } else {
+      socket.emit("start_chat", existing_conversation[0]);
     }
   });
 
+  socket.on("get_messages", async (data, callback) => {
+    const { messages } = await OneToOneMessage.findById(
+      data.conversation_id
+    ).select("messages");
+    callback(messages);
+  });
+
   // handle text/link messages
-  socket.on("text_message", (data) => {
+  socket.on("text_message", async (data) => {
     console.log("Received data", data);
 
     // data(to, from, text)
+    const { to, from, message, conversation_id, type } = data
 
+    const to_user = await User.findById(to)
+    const from_user = await User.findById(from)
+
+    const new_message = {
+      to,
+      from,
+      type,
+      text: message,
+      created_at: Date.now()
+    }
     // create a new convo
-
+    const chat = await OneToOneMessage.findById(conversation_id)
+    chat.messages.push(new_message)
     // save to db
-
+    await chat.save({})
     // emit event
+    io.to(to_user.socket_id).emit("new_message", {
+      conversation_id,
+      message: new_message,
+    })
+    io.to(from_user.socket_id).emit("new_message", {
+      conversation_id,
+      message: new_message,
+    })
   });
 
   // handle media/document messages
